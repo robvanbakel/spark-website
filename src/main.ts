7d05bf8f898a05b400b3bfd4769d697ab3189871
Rob van Bakel <robbakel@gmail.com>
2024-03-18 22:05:25 +0100

    Add Vite to project

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
+import './assets/style.scss'
+
 // ----------------------
 // Screenshot View Picker
 // ----------------------
 
-const viewPickerBlocks = document.querySelectorAll('section#showcase .view-picker .block')
+const viewPickerBlocks = document.querySelectorAll<HTMLDivElement>('section#showcase .view-picker .block')
 
 viewPickerBlocks.forEach((block) => {
   // Add click listener to all blocks
-  block.addEventListener('click', function () {
+  block.addEventListener('click', () => {
     // Remove active class from all blocks
     viewPickerBlocks.forEach((block) => block.classList.remove('active'))
 
     // Add active class to selected block
-    this.classList.add('active')
+    block.classList.add('active')
 
     // Change image to represent selected block
-    document.querySelector('section#showcase .view-display img').src = `./img/view_${this.dataset.view}.jpg`
+    document.querySelector<HTMLImageElement>('section#showcase .view-display img')!.src = `/view_${block.dataset.view}.jpg`
   })
 })
 
@@ -23,16 +25,17 @@ viewPickerBlocks.forEach((block) => {
 // -------------------
 
 const formCTA = document.querySelector('section#cta form')
-const inpCTA = document.querySelector('section#cta form input')
+const inpCTA = document.querySelector<HTMLInputElement>('section#cta form input')
 const successCTA = document.querySelector('section#cta span#success')
 
-formCTA.addEventListener('submit', (e) => {
+formCTA?.addEventListener('submit', (e) => {
   e.preventDefault()
 
   // Send email notification
+  if(!inpCTA) return;
   fetch(`https://api.getping.info/OdfuoZiX/${inpCTA.value}`)
 
   // Hide form and show succes message
   formCTA.classList.add('hide')
-  successCTA.classList.remove('hide')
+  successCTA?.classList.remove('hide')
 })
